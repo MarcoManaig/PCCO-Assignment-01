7801f374d7ba17d6833613b90b0576331cad4f1f
MarcoManaig <158665286+MarcoManaig@users.noreply.github.com>
2024-02-16 05:40:42 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    double bills = 0.01;
+
+    if(bills == 0.01){
+      Console.WriteLine("No Person");
+    }
+    else{
+      Console.WriteLine("Invalid Denomination");
+    }
+  }
+}
